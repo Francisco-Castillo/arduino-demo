d39c88bdf9ed8b0fd084893c1195018ced5c582b
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2022-01-24 19:01:14 -0300

    feat comunicacion serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,44 @@
 #include <Arduino.h>
 
+int led = 13;
+int retardo = 1000;
+char caracter;
+
+void obtenerValorRetardo(char caracter){
+  switch (caracter) {
+    case 'r':
+      retardo = 500;
+      Serial.println("Rapido");
+      break;
+
+    case 'i':
+      retardo = 2000;
+      Serial.println("Lento");
+      break;
+
+    case 'd':
+      retardo = 1000;
+      Serial.println("Defecto");
+      break;
+  }
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available()>0) {
+    caracter = Serial.read();
+
+    obtenerValorRetardo(caracter);
+
+    digitalWrite(led, HIGH);
+    delay(retardo);
+    digitalWrite(led, LOW);
+    delay(retardo);
+
+    Serial.println(retardo);
+  }
 }
